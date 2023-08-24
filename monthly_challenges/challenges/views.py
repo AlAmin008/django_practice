8970341e3340e6339d2e08db37872fdca2ffd7c9
AlAmin008 <piashpiash222@gmail.com>
2023-08-24 16:35:57 +0600

    templates and static files

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,33 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import Http404,HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from django.template.loader import render_to_string
 
 month_challenge = {
     "january" : "This is January.....",
     "february" : "This is February",
     "march" : "This is March........",
     "april" : "This is April",
-    "may" : "This is May......"
+    "may" : "This is May......",
+    "december" : None
 }
 
 # Create your views here.
 
 def index(request):
-    list_items =""
     months = list(month_challenge.keys())
-    for month in months:
-        month_path = reverse("month-challenge", args=[month])
-        list_items+= f"<li><a href=\"{month_path}\">"f"<h1>{month.capitalize()}</h1></a></li>"
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
+    return render(request,"challenges/index.html",{
+        "month_list" : months
+    })
 
 def monthly_challenges(request,month):
     try:
         challenge_text = month_challenge[month]
-        response_data = f"<h1>{challenge_text}<h1>"
-        return HttpResponse(response_data)
+        return render(request, "challenges/challenge.html" , {
+            "text" : challenge_text,
+            "month" : month
+        })
     except:
-        return HttpResponseNotFound("<h1>This Page is not Valid<h1>")
+        return Http404
 
 def monthly_challenges_by_number(request,month):
     month_name= list(month_challenge.keys())
